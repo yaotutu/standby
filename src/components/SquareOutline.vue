d56a0979e8cc83f4d3f6f4ba770edd8139bc0f25
yaotutu <957810668@qq.com>
2023-06-21 09:19:24 +0000

    .

---
+++ src/components/SquareOutline.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="Square-outline-wrap"
+    :style="{ backgroundColor: backgroundColor }"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  backgroundColor: {
+    type: String,
+    default: "black",
+  },
+});
+console.log(props);
+</script>
+
+<style lang="css" scoped>
+.Square-outline-wrap {
+  width: 40vh;
+  height: 40vh;
+  border-radius: 50px;
+  padding: 30px;
+}
+</style>
--- src/layout/Layout1/index.vue
+++ src/layout/Layout1/index.vue
@@ -1,14 +1,6 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  </div>
+  <Weather />
 </template>
 <script setup lang="ts">
-import { getWeatherMinutely } from "@/api/weather";
-import { onMounted } from "vue";
-
-onMounted(() => {
-  getWeatherMinutely();
-});
+import Weather from "@/components/weather/index.vue";
 </script>
