92e4c484138d22d67268e39d6012ed569932d4d8
yaotutu <957810668@qq.com>
2023-06-29 11:53:23 +0000

    add icons

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "@/styles/index.css";
+import "qweather-icons/font/qweather-icons.css";
 import router from "./router";
-
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
