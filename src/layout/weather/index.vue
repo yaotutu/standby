90da97c5b140a2293d7efc90af100e30d9cad868
yaotutu <957810668@qq.com>
2023-06-29 11:53:26 +0000

    .

---
+++ src/layout/weather/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>实时天气</h1>
+    It's raining <i class="qi-307"></i> and cold <i class="qi-cold"></i>
+
+    <ul>
+      <li>温度</li>
+      <li>天气概览</li>
+    </ul>
+  </div>
+</template>
+<script setup lang="ts">
+import { getWeatherNow } from "@/api/weather";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  getWeatherNow().then((res) => {
+    console.log(res);
+  });
+});
+</script>
+<style></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import Weather from "@/layout/Weather.vue";
-import Square from "@/layout/Square.vue";
+import Weather from "@/layout/weather/index.vue";
+import Square from "@/layout/square/index.vue";
 
 const router = createRouter({
   history: createWebHashHistory(),
