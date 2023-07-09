8c9da2f4b36df84deaf1d1a337b8d783e047c4de
yaotutu <957810668@qq.com>
2023-07-09 14:19:38 +0800

    .

--- src/layout/standby/index.vue
+++ src/layout/standby/index.vue
@@ -8,34 +8,6 @@
   >
     <DigitalClock :timeFormat="TimeFormats.HourMinuteSecond" />
   </div>
-  <van-popup
-    v-model:show="showSetting"
-    position="right"
-    :style="{ width: '40%', height: '100%' }"
-    @click-overlay="onClickOverlay"
-  >
-    <div class="settingBox">
-      <div class="flex flex-initial flex-col">
-        <div class="">字体颜色选择</div>
-        <div
-          class="flex flex-row flex-initial overscroll-auto w-full overflow-auto"
-        >
-          <div
-            v-for="item in colorList"
-            :style="{
-              backgroundColor: item.value,
-              width: '50px',
-              height: '50px',
-              flexShrink: 0,
-              borderRadius: '50%',
-            }"
-          >
-            12222
-          </div>
-        </div>
-      </div>
-    </div>
-  </van-popup>
 </template>
 
 <script setup lang="ts">
@@ -45,15 +17,11 @@ import { TimeFormats } from "@/enums";
 import { useDigitalClockStore } from "@/stores/index";
 import { storeToRefs } from "pinia";
 const store = useDigitalClockStore();
-const { backgroundColor, fontColor, colorList } = storeToRefs(store);
+const { backgroundColor, fontColor } = storeToRefs(store);
 const showSetting = ref(true);
 const handleDoubleClick = () => {
   // 打开设置页面
   showSetting.value = true;
-};
-const onClickOverlay = () => {
-  // 关闭设置页面
-  showSetting.value = false;
 };
 </script>
 
@@ -62,6 +30,7 @@ const onClickOverlay = () => {
   font-family: "Lobster-Regular";
   src: url("@/assets/fonts/Lobster-Regular.ttf") format("truetype");
 }
+
 .box {
   width: 100%;
   height: 100%;
@@ -73,14 +42,5 @@ const onClickOverlay = () => {
   background-color: v-bind(backgroundColor);
   text-shadow: 0 0 10px #000;
   border-radius: 10px;
-}
-.configPage .el-dialog__header {
-  display: none;
-}
-.settingBox {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  overflow: hidden;
 }
 </style>
